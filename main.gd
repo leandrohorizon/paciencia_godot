7fdacf48d3164eae1d260b2a725c3d29bdaaccc5
leandrohorizon <leandro.mcontato@gmail.com>
2025-04-28 19:05:55 -0300

    Implementa a lógica de criação e entrega de cartas no jogo. Adiciona funções para criar um baralho, distribuir cartas entre os tableau e o deck. A estrutura do código foi organizada para facilitar a manipulação das cartas durante o jogo.

--- main.gd
+++ main.gd
@@ -1,9 +1,43 @@
 extends Node2D
 
+var deck = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	create_deck()
+	deliver_cards()
 
+func create_deck() -> void:
+	var suits = ["hearts", "diamonds", "clubs", "spades"]
+	var values = range(1, 14) # 1 a 13 (Ás a Rei)
+	
+	for suit in suits:
+		for value in values:
+			var card = preload("res://card.tscn").instantiate()
+			card.setup(value, suit)
+			deck.append(card)
+
+func deliver_cards() -> void:
+	deliver_in_tableaus();
+	deliver_in_deck();
+	
+func deliver_in_tableaus() -> void:
+	var tableaus = [
+		$tableau1, $tableau2, $tableau3,
+		$tableau4, $tableau5, $tableau6,
+		$tableau7
+	]
+
+	var quantity = 1
+
+	for tableau in tableaus:
+		for index in range(0, quantity):
+			tableau.add_child(deck.pop_back())
+		quantity = quantity + 1
+
+func deliver_in_deck() -> void:
+	for card in deck:
+		$deck.add_child(card)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
