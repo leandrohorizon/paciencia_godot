4af998fa8c928c003c5145ebc2dd4694f1c767a5
leandrohorizon <leandro.mcontato@gmail.com>
2025-05-04 18:22:01 -0300

    Atualiza a função de entrada em reset.gd para iniciar o jogo apenas em toques na tela, melhorando a responsividade e a interatividade do usuário ao iniciar o jogo.

--- reset.gd
+++ reset.gd
@@ -6,5 +6,6 @@ func _ready() -> void:
 	$Area2D.input_event.connect(_on_area2d_input_event)
 
 func _on_area2d_input_event(viewport, event, shape_idx):
-	var main = get_node("/root/Main/")
-	main.start_game()
+	if event is InputEventScreenTouch and event.pressed:
+		var main = get_node("/root/Main/")
+		main.start_game()
